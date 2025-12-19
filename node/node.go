7226facd5aaf84c489a221b79be386034464ed65
Igor Sirotin <igor.sirotin.1012@gmail.com>
2025-12-19 20:10:02 +0000

    fix: graceful shutdown (#11)
    * chore: minor logs cleanup
    
    * fix: graceful shutdown
    
    * chore: ignore ds store

--- node/node.go
+++ node/node.go
@@ -9,6 +9,7 @@ import (
 	"encoding/hex"
 	"fmt"
 	"math/rand"
+	"sync"
 	"sync/atomic"
 	"time"
 
@@ -54,6 +55,7 @@ type Node struct {
 	epoch  int64
 	ctx    context.Context
 	cancel context.CancelFunc
+	wg     sync.WaitGroup
 
 	store     store.MessageStore
 	transport transport.Transport
@@ -172,7 +174,7 @@ func NewEphemeralNode(
 		payloads:  newPayloads(),
 		nextEpoch: nextEpoch,
 		epoch:     currentEpoch,
-		logger:    logger.With(zap.Namespace("mvds")),
+		logger:    logger.Named("mvds"),
 		mode:      mode,
 	}
 }
@@ -205,7 +207,7 @@ func NewNode(
 		nextEpoch: nextEpoch,
 		ID:        id,
 		epoch:     currentEpoch,
-		logger:    logger.With(zap.Namespace("mvds")),
+		logger:    logger.Named("mvds"),
 		mode:      mode,
 	}
 }
@@ -216,24 +218,30 @@ func (n *Node) CurrentEpoch() int64 {
 
 // Start listens for new messages received by the node and sends out those required every epoch.
 func (n *Node) Start(duration time.Duration) {
+	n.wg.Add(1)
 	go func() {
+		defer n.wg.Done()
 		for {
-			select {
-			case <-n.ctx.Done():
-				n.logger.Info("Watch stopped")
+			p, ok := n.transport.Watch(n.ctx)
+			if !ok {
+				n.logger.Debug("watching transport stopped")
 				return
-			default:
-				p := n.transport.Watch()
-				go n.onPayload(p.Sender, p.Payload)
 			}
+			n.wg.Add(1)
+			go func() {
+				defer n.wg.Done()
+				n.onPayload(p.Sender, p.Payload)
+			}()
 		}
 	}()
 
+	n.wg.Add(1)
 	go func() {
+		defer n.wg.Done()
 		for {
 			select {
 			case <-n.ctx.Done():
-				n.logger.Info("reset data sync for peer stopped")
+				n.logger.Debug("reset data sync for peer stopped")
 				return
 			case event := <-n.peerStatusChangeEvent:
 				if event.Status == OnlineStatus && event.EventTime > uint64(time.Now().Unix())-FreshEventPeriod {
@@ -244,21 +252,22 @@ func (n *Node) Start(duration time.Duration) {
 		}
 	}()
 
+	n.wg.Add(1)
 	go func() {
+		defer n.wg.Done()
 		for {
 			select {
 			case <-n.ctx.Done():
-				n.logger.Info("Epoch processing stopped")
+				n.logger.Debug("epoch processing stopped")
 				return
-			default:
-				time.Sleep(duration)
+			case <-time.After(duration):
 				err := n.sendMessages()
 				if err != nil {
-					n.logger.Error("Error sending messages.", zap.Error(err))
+					n.logger.Error("error sending messages.", zap.Error(err))
 				}
 				err = n.syncState.Clear(MaxSendCount)
 				if err != nil {
-					n.logger.Error("Error clearing sync state.", zap.Error(err))
+					n.logger.Error("error clearing sync state.", zap.Error(err))
 				}
 				atomic.AddInt64(&n.epoch, 1)
 				// When a persistent node is used, the epoch needs to be saved.
@@ -274,9 +283,10 @@ func (n *Node) Start(duration time.Duration) {
 
 // Stop message reading and epoch processing
 func (n *Node) Stop() {
-	n.logger.Info("Stopping node")
+	n.logger.Info("stopping node")
 	n.Unsubscribe()
 	n.cancel()
+	n.wg.Wait()
 }
 
 // Subscribe subscribes to incoming messages.
@@ -393,7 +403,7 @@ func (n *Node) sendMessages() error {
 
 			msg, err := n.store.Get(m)
 			if err != nil {
-				n.logger.Error("Failed to retreive message",
+				n.logger.Error("failed to retreive message",
 					zap.String("messageID", hex.EncodeToString(m[:4])),
 					zap.Error(err),
 				)
